a5ad905fd892cf0fe980b8b7fd4f80f68f2356de
whyang666 <whyang666@gmail.com>
2017-05-07 03:33:11 +0800

    Initial commit

---
+++ src/java/com/buptsse/spm/dao/IExamDao.java
@@ -0,0 +1,26 @@
+package com.buptsse.spm.dao;
+
+import com.buptsse.spm.domain.Exam;
+
+import java.util.List;
+
+
+/**
+ * @author BUPT-TC
+ * @date 2015年11月23日 下午2:46
+ * @description	在线测试持久层接口定义
+ * @modify BUPT-TC
+ * @modifyDate 
+ */
+
+public interface IExamDao {
+	public boolean saveExam(Exam exam);
+	public boolean updateExam(Exam exam);
+	public boolean deleteExam(Exam exam);
+	public List<Exam> findExam(String hql,Object[] param);
+	public List<Exam> findExam(String hql,List param);
+	public boolean saveOrUpdateExam(Exam exam);
+	public Long countExam(String hql,List param);
+	public List findPage(String hql,List param , Integer page,Integer rows);	
+
+}
